5e057950330f5e8439b204b5a1f2c15c09afaafb
Patrick Crumley <patrick.crumley@gmail.com>
2021-04-07 21:22:07 -0400

    create spectral -> spatial function

--- src/flds.rs
+++ src/flds.rs
@@ -114,6 +114,32 @@ impl Fld {
             }
         }
     }
+
+    #[inline(always)]
+    pub fn copy_to_spatial(&mut self, sim: &Sim) -> () {
+        // assert stuff about ghost zones etc.
+        let spatial = &mut self.spatial;
+        let spectral = &self.spectral;
+        if !cfg!(feature = "unchecked") {
+            assert!(spatial.len() == (sim.size_x + 2) * (sim.size_y + 2));
+            assert!(spectral.len() == sim.size_x * sim.size_y);
+        }
+        let size_x = sim.size_x;
+        let size_y = sim.size_y;
+        for iy in 0..size_y {
+            let ij = iy * (size_x);
+            let ij_ghosts = (iy + 1) * (size_x + 2);
+            for ix in 0..size_x {
+                unsafe {
+                    // completely safe due to assert above... unless unchecked is run.
+                    // still should be fine but not guaranteed at runtime.
+                    *spatial.get_unchecked_mut(ij_ghosts + ix + 1) =
+                        spectral.get_unchecked(ij + ix).re;
+                }
+            }
+        }
+        Flds::update_ghosts(sim, spatial);
+    }
 }
 
 impl Flds {
